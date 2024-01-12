904737a984ad42d37ae5e51cf5a0e94cbaf3bb9b
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-12 17:01:05 +0530

    updates

---
+++ client/src/context/SocketProvider.jsx
@@ -0,0 +1,9 @@
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
