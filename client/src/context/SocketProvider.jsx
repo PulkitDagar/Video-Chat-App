368a46b22bb221d251a4dad2e71ae9f581200aba
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-12 17:01:42 +0530

    updates

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -7,3 +7,13 @@ export const useSocket = () => {
   const socket = useContext(SocketContext);
   return socket;
 };
+
+export const SocketProvider = (props) => {
+    const socket = useMemo(() => io("localhost:8000"), []);
+  
+    return (
+      <SocketContext.Provider value={socket}>
+        {props.children}
+      </SocketContext.Provider>
+    );
+  };
