f151e2bd89a40550e0ca228fef6794f9a498b3b5
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-13 18:03:28 +0530

    update

---
+++ client/src/screens/Lobby.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSocket } from "../context/SocketProvider";
+
+const LobbyScreen = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const socket = useSocket();
+  const navigate = useNavigate();
