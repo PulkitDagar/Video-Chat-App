5339c76734e69ba99c78e05be5927cd24c7c74bf
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-13 18:08:52 +0530

    update

--- client/src/service/peer.js
+++ client/src/service/peer.js
@@ -13,3 +13,12 @@ class PeerService {
         });
       }
     }
+
+    async getAnswer(offer) {
+        if (this.peer) {
+          await this.peer.setRemoteDescription(offer);
+          const ans = await this.peer.createAnswer();
+          await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+          return ans;
+        }
+      }
