97bbc590e696e86be6db14a7b49cb837a90db6b3
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-14 22:31:27 +0530

    update

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -9,3 +9,8 @@ const RoomPage = () => {
     const [remoteSocketId, setRemoteSocketId] = useState(null);
     const [myStream, setMyStream] = useState();
     const [remoteStream, setRemoteStream] = useState();
+
+    const handleUserJoined = useCallback(({ email, id }) => {
+        console.log(`Email ${email} joined room`);
+        setRemoteSocketId(id);
+      }, []);
