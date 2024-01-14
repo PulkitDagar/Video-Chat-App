dfe7ad34335c71e5aabe5f573d0bc5f372dfa6ef
Pulkit Dagar <dagarpulkit142@gmail.com>
2024-01-14 22:30:53 +0530

    updates

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -2,3 +2,10 @@ import React, { useEffect, useCallback, useState } from "react";
 import ReactPlayer from "react-player";
 import peer from "../service/peer";
 import { useSocket } from "../context/SocketProvider";
+
+
+const RoomPage = () => {
+    const socket = useSocket();
+    const [remoteSocketId, setRemoteSocketId] = useState(null);
+    const [myStream, setMyStream] = useState();
+    const [remoteStream, setRemoteStream] = useState();
